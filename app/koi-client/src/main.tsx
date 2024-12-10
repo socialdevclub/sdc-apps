46c22ecf676ebd4e33dfdeac252c853e6cc34dfb
haian <ada.lovelace@kakao.com>
2024-12-10 20:56:22 +0900

    fix: Remove TypeScript error suppression comment in main.tsx

--- app/koi-client/src/main.tsx
+++ app/koi-client/src/main.tsx
@@ -4,6 +4,5 @@ import Global from './Global';
 const selected = document.getElementById('root');
 if (selected) {
   const root = createRoot(selected);
-  // @ts-expect-error 왜 에러나지?
   root.render(<Global />);
 }
