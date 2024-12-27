6c24b578a71486f2023ef374c4065c3107bcf152
haian <ada.lovelace@kakao.com>
2024-12-28 01:36:43 +0900

    feat: Update PartyList component to conditionally render cards based on activity status
    - Modified the PartyList component to only display party cards for parties that are not closed (activityId !== 'CLOSE').
    - This change improves the user experience by preventing the display of inactive parties in the list.

--- app/koi-client/src/page/@/component/PartyList.tsx
+++ app/koi-client/src/page/@/component/PartyList.tsx
@@ -18,18 +18,20 @@ const PartyList = () => {
 
   return partyList.map((party) => {
     return (
-      <Card key={party._id} title={party.title}>
-        <Button
-          type="primary"
-          onClick={async () => {
-            await joinParty({ partyId: party._id, userId: supabaseSession.user.id });
-            navigate(`/party/${party._id}`);
-          }}
-          // disabled={party.publicScope !== 'PUBLIC'}
-        >
-          참가
-        </Button>
-      </Card>
+      party.activityId !== 'CLOSE' && (
+        <Card key={party._id} title={party.title}>
+          <Button
+            type="primary"
+            onClick={async () => {
+              await joinParty({ partyId: party._id, userId: supabaseSession.user.id });
+              navigate(`/party/${party._id}`);
+            }}
+            // disabled={party.publicScope !== 'PUBLIC'}
+          >
+            참가
+          </Button>
+        </Card>
+      )
     );
   });
 };
