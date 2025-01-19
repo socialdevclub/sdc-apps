e4dfd64bda2113145e4596b18dd8f19fb26ed200
s2oy <w2032@e-mirim.hs.kr>
2025-01-19 23:38:23 +0900

    fix: discord 초대 링크 상수화

--- app/stock-landing/src/page/home/index.tsx
+++ app/stock-landing/src/page/home/index.tsx
@@ -1,14 +1,13 @@
 import { Style } from './index.style';
 
+const DISCORD_INVITE_LINK = 'https://discord.gg/H8sq77NabR';
 const Home = () => {
   return (
     <Style.Container>
       <Style.Header>
         <Style.Title>소셜데브클럽</Style.Title>
         <Style.Subtitle>사람과 사람을 연결시켜주는 소셜 게임을 함께 만들고 즐겨요 👾</Style.Subtitle>
-        <Style.Button onClick={() => window.open('https://discord.gg/H8sq77NabR', '_blank')}>
-          디스코드 입장하기
-        </Style.Button>
+        <Style.Button onClick={() => window.open(DISCORD_INVITE_LINK, '_blank')}>디스코드 입장하기</Style.Button>
       </Style.Header>
     </Style.Container>
   );
