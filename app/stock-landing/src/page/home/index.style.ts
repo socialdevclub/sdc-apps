34aef2561151981f5f6abae09a3ea537d4a9fc72
s2oy <w2032@e-mirim.hs.kr>
2025-01-19 23:29:01 +0900

    fix: 캐러셀 UI 삭제

--- app/stock-landing/src/page/home/index.style.ts
+++ app/stock-landing/src/page/home/index.style.ts
@@ -1,9 +1,6 @@
-/** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
 
-const Container = styled.div`
-  color: #fff;
-  background: #111;
+const Container = styled.div`s
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -20,23 +17,17 @@ const Header = styled.header`
 const Title = styled.h1`
   font-size: 3rem;
   margin-bottom: 0.5rem;
-
-  span {
-    color: #29d3ff;
-  }
 `;
 
 const Subtitle = styled.p`
   font-size: 1.2rem;
   margin-bottom: 1.5rem;
-  color: #ddd;
 `;
 
 const Button = styled.button`
   background: linear-gradient(90deg, #4413bf, #29d3ff);
   border: none;
   border-radius: 24px;
-  color: #fff;
   font-size: 1rem;
   font-weight: bold;
   padding: 12px 24px;
@@ -47,54 +38,9 @@ const Button = styled.button`
   }
 `;
 
-const FeatureBox = styled.div`
-  overflow: hidden;
-  height: 120px; /* 고정 높이 */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 100%;
-  max-width: 500px;
-  border-radius: 12px;
-  background: #222;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
-  padding: 1rem;
-`;
-
-const FeatureText = styled.div`
-  position: absolute;
-  width: 100%;
-  text-align: center;
-  font-size: 1.3rem;
-  color: #ddd;
-  animation: slide 3s infinite;
-
-  @keyframes slide {
-    0% {
-      transform: translateY(100%);
-      opacity: 0;
-    }
-    10% {
-      transform: translateY(0);
-      opacity: 1;
-    }
-    90% {
-      transform: translateY(0);
-      opacity: 1;
-    }
-    100% {
-      transform: translateY(-100%);
-      opacity: 0;
-    }
-  }
-`;
-
 export const Style = {
   Button,
   Container,
-  FeatureBox,
-  FeatureText,
   Header,
   Subtitle,
   Title,
