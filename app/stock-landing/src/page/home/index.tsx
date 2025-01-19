5ab812372dbf0b860d1efd57dff093f5db3e6a43
s2oy <w2032@e-mirim.hs.kr>
2025-01-19 23:29:34 +0900

    fix: 소셜데브클럽 단어간의 공백 삭제

--- app/stock-landing/src/page/home/index.tsx
+++ app/stock-landing/src/page/home/index.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <Style.Container>
       <Style.Header>
-        <Style.Title>소셜 데브 클럽</Style.Title>
+        <Style.Title>소셜데브클럽</Style.Title>
         <Style.Subtitle>게임을 통해 사람들과 연결되고 함께 성장하는 특별한 경험을 제공합니다.</Style.Subtitle>
         <Style.Button onClick={() => window.open('https://discord.gg/H8sq77NabR', '_blank')}>
           디스코드로 가입하기
