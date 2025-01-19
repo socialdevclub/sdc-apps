ad0ee6f6811c10bc4291ad08478768615ad37b51
s2oy <w2032@e-mirim.hs.kr>
2025-01-19 23:31:39 +0900

    fix: 문구 톤앤매너 개선

--- app/stock-landing/src/page/home/index.tsx
+++ app/stock-landing/src/page/home/index.tsx
@@ -5,9 +5,9 @@ const Home = () => {
     <Style.Container>
       <Style.Header>
         <Style.Title>소셜데브클럽</Style.Title>
-        <Style.Subtitle>게임을 통해 사람들과 연결되고 함께 성장하는 특별한 경험을 제공합니다.</Style.Subtitle>
+        <Style.Subtitle>사람과 사람을 연결시켜주는 소셜 게임을 함께 만들고 즐겨요 👾</Style.Subtitle>
         <Style.Button onClick={() => window.open('https://discord.gg/H8sq77NabR', '_blank')}>
-          디스코드로 가입하기
+          디스코드 입장하기
         </Style.Button>
       </Style.Header>
     </Style.Container>
