12ce25d4082bd65ca82f57aec13bd5c473df2539
jangseyeong <jang.se.yeong000@gmail.com>
2025-03-05 21:39:18 +0900

    feat: Header 컴포넌트에 CenterComponent Prop 추가

--- app/koi-client/src/component-presentation/Header.tsx
+++ app/koi-client/src/component-presentation/Header.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@linaria/react';
 import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { UserRound } from 'lucide-react';
 import React from 'react';
 
 interface AvatarProp {
@@ -16,33 +16,47 @@ type Props = {
   | {
       avatar?: AvatarProp;
       LeftComponent?: never;
+      CenterComponent?: never;
     }
   | {
       avatar?: never;
       LeftComponent?: React.ReactNode;
+      CenterComponent?: never;
+    }
+  | {
+      avatar?: AvatarProp;
+      LeftComponent?: never;
+      CenterComponent?: React.ReactNode;
     }
 );
 
-const Header = ({ title, avatar = { isVisible: false }, LeftComponent, RightComponent }: Props) => {
+const Header = ({ title, avatar = { isVisible: false }, CenterComponent, LeftComponent, RightComponent }: Props) => {
   const { isVisible, src, onClick } = avatar;
 
   return (
     <Container>
       <LeftSection>
-        <AvatarWrapper>
-          {LeftComponent || (
-            <Avatar
-              size="large"
-              style={{ cursor: onClick ? 'pointer' : 'default', visibility: isVisible ? 'visible' : 'hidden' }}
-              icon={<UserOutlined />}
-              src={src}
-              onClick={onClick}
-            />
-          )}
-        </AvatarWrapper>
+        {LeftComponent || (
+          <Avatar
+            size="large"
+            style={{
+              alignItems: 'center',
+              backgroundColor: '#4B5563',
+              color: '#D1D5DB',
+              cursor: onClick ? 'pointer' : 'default',
+              display: 'flex',
+              justifyContent: 'center',
+              visibility: isVisible ? 'visible' : 'hidden',
+            }}
+            icon={<UserRound />}
+            src={src}
+            onClick={onClick}
+          />
+        )}
         <Title>{title}</Title>
       </LeftSection>
-      <RIghtSection>{RightComponent}</RIghtSection>
+      {CenterComponent && <CenterSection>{CenterComponent}</CenterSection>}
+      <RightSection>{RightComponent}</RightSection>
     </Container>
   );
 };
@@ -54,27 +68,33 @@ const Title = styled.div`
 `;
 
 const LeftSection = styled.div`
+  flex: 1;
   display: flex;
   align-items: center;
   gap: 4px;
 `;
 
-const RIghtSection = styled.div`
-  flex: 80px;
+const CenterSection = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const RightSection = styled.div`
+  flex: 1;
   display: flex;
   justify-content: flex-end;
 `;
 
 const Container = styled.div`
   position: relative;
-  padding: 24px 12px;
+  padding: 16px 16px;
   box-sizing: border-box;
   display: flex;
   align-items: center;
-`;
-
-const AvatarWrapper = styled.div`
-  flex: 80px;
+  justify-content: space-between;
+  color: white;
 `;
 
 export default Header;
