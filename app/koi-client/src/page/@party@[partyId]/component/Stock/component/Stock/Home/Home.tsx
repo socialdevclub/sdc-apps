9f922e41166d14ff45c557e9effc74be9a5e57af
HyunJun CHOI <107416133+solssak@users.noreply.github.com>
2025-03-08 16:09:12 +0900

    feat: 정보 탭 전체 & 홈탭-내 예측 정보 구현 (#33)
    ## 변경 사항
    
    ### 주식게임 - 정보
    #### 앞으로의 정보 & 지난 정보
    - 1분 단위 갱신
    - 앞으로의 정보 1분 남았을 때 - "🚨임박"
    - 지난 정보 1분 지났을 때 - "방금 전"
    - 이외 -"13분 후", "13분 전"
    #### 정보뽑기
    - UI 구현
    
    |임박|방금전|정보 뽑기|
    |---|---|---|
    |<img width="441" alt="Screenshot 2025-03-08 at 02 41 11"
    src="https://github.com/user-attachments/assets/8cff82d5-a760-4efb-a441-96725393997b"
    />|<img width="444" alt="Screenshot 2025-03-08 at 02 41 32"
    src="https://github.com/user-attachments/assets/a7478eaa-827e-45ed-9f9b-0a3fcccb3ba3"
    />|<img width="506" alt="Screenshot 2025-03-08 at 04 24 31"
    src="https://github.com/user-attachments/assets/6f3e52c3-911d-455d-b406-530a01f83b9f"
    />|
    
    ### 주식게임 - 홈(내 예측 정보)
    |정보가 존재할 때|더 이상 정보가 존재하지 않을 때|
    |---|---|
    |<img width="440" alt="Screenshot 2025-03-08 at 02 39 20"
    src="https://github.com/user-attachments/assets/c6c84808-cb07-48ea-a596-2177b04a8cb2"
    />|<img width="441" alt="Screenshot 2025-03-08 at 02 44 16"
    src="https://github.com/user-attachments/assets/d98b32b2-66b6-4943-a296-aa676cf013a1"
    />|

--- app/koi-client/src/page/@party@[partyId]/component/Stock/component/Stock/Home/Home.tsx
+++ app/koi-client/src/page/@party@[partyId]/component/Stock/component/Stock/Home/Home.tsx
@@ -1,14 +1,21 @@
 import { Flex } from 'antd';
 import { useAtomValue } from 'jotai';
-import { commaizeNumber } from '@toss/utils';
+import { commaizeNumber, objectEntries } from '@toss/utils';
 import styled from '@emotion/styled';
+import { getDateDistance } from '@toss/date';
+import dayjs from 'dayjs';
+import { useEffect, useRef, useState } from 'react';
+import { css } from '@emotion/react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { UserStore } from '../../../../../../../store';
 import { Query } from '../../../../../../../hook';
-import DrawStockInfo from '../DrawInfo';
 import { MyLevel } from './MyLevel';
 import Card from '../../../../../../../component-presentation/Card';
 import * as COLOR from '../../../../../../../config/color';
 import StartLoan from '../StartLoan';
+import prependZero from '../../../../../../../service/prependZero';
+import InfoBox from '../../../../../../../component-presentation/InfoBox';
+import { colorDown, colorUp } from '../../../../../../../config/color';
 
 const getProfitRatio = (v: number) => ((v / 1000000) * 100 - 100).toFixed(2);
 
@@ -16,19 +23,84 @@ interface Props {
   stockId: string;
 }
 
+const getFormattedGameTime = (startTime?: string) => {
+  if (!startTime) return '00:00';
+
+  return `${prependZero(getDateDistance(dayjs(startTime).toDate(), new Date()).minutes, 2)}:${prependZero(
+    getDateDistance(dayjs(startTime).toDate(), new Date()).seconds,
+    2,
+  )}`;
+};
+
 const Home = ({ stockId }: Props) => {
   const supabaseSession = useAtomValue(UserStore.supabaseSession);
   const userId = supabaseSession?.user.id;
 
-  const { data: stock } = Query.Stock.useQueryStock(stockId);
+  const { data: stock, refetch } = Query.Stock.useQueryStock(stockId);
   const { data: users } = Query.Stock.useUserList(stockId);
   const { user } = Query.Stock.useUser({ stockId, userId });
 
   const { allSellPrice, allUserSellPriceDesc } = Query.Stock.useAllSellPrice({ stockId, userId });
+  const [gameTime, setGameTime] = useState(getFormattedGameTime(stock?.startedTime));
+  const gameTimeRef = useRef(gameTime);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handlePageChange = () => {
+    const params = new URLSearchParams(location.search);
+    params.set('page', '정보');
+    navigate(`${location.pathname}?${params.toString()}`, { replace: true });
+  };
+
+  useEffect(() => {
+    if (!stock?.startedTime) return () => {};
+
+    const interval = setInterval(() => {
+      const newGameTime = getFormattedGameTime(stock.startedTime);
+
+      if (newGameTime !== gameTimeRef.current) {
+        const newGameMinute = parseInt(newGameTime.split(':')[0], 10);
+        const lastGameMinute = parseInt(gameTimeRef.current.split(':')[0], 10);
+
+        gameTimeRef.current = newGameTime;
+        setGameTime(newGameTime);
+
+        if (newGameMinute !== lastGameMinute) {
+          refetch();
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [stock?.startedTime, refetch]);
 
   if (!user || !stock) {
     return <div>불러오는 중.</div>;
   }
+
+  const gameTimeInSeconds = parseInt(gameTime.split(':')[0], 10) * 60 + parseInt(gameTime.split(':')[1], 10);
+  const gameTimeInMinutes = Math.ceil(parseInt(gameTime.split(':')[0], 10));
+
+  const myInfos = objectEntries(stock.companies).reduce((myInfos, [company, companyInfos]) => {
+    companyInfos.forEach((companyInfo, idx) => {
+      if (companyInfo.정보.some((name) => name === userId)) {
+        myInfos.push({
+          company,
+          price: idx > 0 ? companyInfo.가격 - companyInfos[idx - 1].가격 : 0,
+          timeIdx: idx,
+        });
+      }
+    });
+    return myInfos;
+  }, [] as Array<{ company: string; timeIdx: number; price: number }>);
+
+  const futureInfos = myInfos
+    .filter((info) => {
+      const infoTimeInSeconds = stock?.fluctuationsInterval && info.timeIdx * 60 * stock.fluctuationsInterval;
+      return infoTimeInSeconds >= gameTimeInSeconds;
+    })
+    .sort((a, b) => a.timeIdx - b.timeIdx);
 
   const allProfitDesc = allUserSellPriceDesc()
     .map(({ userId, allSellPrice }) => {
@@ -89,12 +161,62 @@ const Home = ({ stockId }: Props) => {
             stock.isVisibleRank ? <>{allProfitDesc.findIndex((v) => v.userId === userId) + 1}위</> : <></>
           }
         />
-        <br />
-        <Flex align="center" justify="space-between" gap={4} css={{ width: '100%' }}>
-          <h3>내가 가진 정보</h3>
-          <DrawStockInfo stockId={stockId} />
-        </Flex>
       </Container>
+      <Divider />
+      <Wrapper>
+        <TitleWrapper>
+          <LeftSection>
+            <H3>내 예측 정보</H3>
+            <H6Wrapper>
+              <H6>총 {myInfos.length}개 보유</H6>
+            </H6Wrapper>
+          </LeftSection>
+          <H5 onClick={handlePageChange}>전체보기 &gt;</H5>
+        </TitleWrapper>
+        {futureInfos.length === 0 ? (
+          <H4>현재 시각 이후의 정보가 없습니다</H4>
+        ) : (
+          <H4>현재 시각 이후의 정보 최대 2개가 표시됩니다</H4>
+        )}
+        <FutureInfoWrapper>
+          {futureInfos.slice(0, 2).map(({ company, price, timeIdx }) => {
+            const infoTimeInMinutes = timeIdx * stock.fluctuationsInterval;
+            const remainingTime = infoTimeInMinutes - gameTimeInMinutes;
+
+            return (
+              <InfoBox
+                key={`${company}_${timeIdx}`}
+                title={company}
+                value={`${price >= 0 ? '▲' : '▼'}${commaizeNumber(Math.abs(price))}`}
+                valueColor={price >= 0 ? colorUp : colorDown}
+                leftTime={
+                  <div
+                    css={css`
+                      font-size: 14px;
+                      color: #c084fc;
+                      min-width: 50px;
+                      letter-spacing: 0.5px;
+                    `}
+                  >
+                    {remainingTime <= 1 ? `🚨 임박` : `${remainingTime}분 후`}
+                  </div>
+                }
+                changeTime={
+                  <div
+                    css={css`
+                      font-size: 12px;
+                      color: #9ca3af;
+                      letter-spacing: 0.5px;
+                    `}
+                  >
+                    {prependZero(timeIdx * stock.fluctuationsInterval, 2)}:00
+                  </div>
+                }
+              />
+            );
+          })}
+        </FutureInfoWrapper>
+      </Wrapper>
       <StickyBottom>
         <StartLoan stockId={stockId} />
       </StickyBottom>
@@ -106,10 +228,71 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
   gap: 12px;
-  padding: 12px 0 100px 0;
+  padding: 12px 0 20px 0;
   flex: 1 1 0;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const H3 = styled.h3`
+  font-size: 16px;
+  margin: 0;
+`;
+
+const H5 = styled.h5`
+  font-size: 10px;
+  color: #9ca3af;
+  margin: 0;
+  padding: 8px 8px;
+  cursor: pointer;
+`;
+
+const H4 = styled.h4`
+  font-size: 10px;
+`;
+
+const H6 = styled.h6`
+  font-size: 12px;
+  margin: 0;
+  color: #c084fc;
+`;
+
+const H6Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 8px;
+  background-color: rgba(192, 132, 252, 0.2);
+  border-radius: 20px;
+`;
+
+const FutureInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #374151;
+  margin-bottom: 20px;
 `;
 
 // TODO: 만약 영역이 겹치는 이슈가 발생 시 수정
