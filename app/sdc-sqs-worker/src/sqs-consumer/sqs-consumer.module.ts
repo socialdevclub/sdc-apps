f02acaf07688a3aabc9843a00dddc29be0ab76bf
haian <ada.lovelace@kakao.com>
2025-03-12 23:01:48 +0900

    feat: SQS 소비자 모듈에 StockModule 추가
    주식 거래 기능을 지원하기 위해 SqsConsumerModule에 StockModule을 추가하였습니다. 이를 통해 주식 관련 서비스와의 통합이 원활해졌습니다.

--- app/sdc-sqs-worker/src/sqs-consumer/sqs-consumer.module.ts
+++ app/sdc-sqs-worker/src/sqs-consumer/sqs-consumer.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from 'feature-nest-stock';
+import { StockModule, UserModule } from 'feature-nest-stock';
 import { SqsConsumerService } from './sqs-consumer.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, StockModule],
   providers: [SqsConsumerService],
 })
 export class SqsConsumerModule {}
