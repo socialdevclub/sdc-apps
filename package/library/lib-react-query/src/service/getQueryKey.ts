38efcd2d405e691af2da5ad7a44b5df5152a6e67
haian <ada.lovelace@kakao.com>
2025-03-23 03:47:34 +0900

    feat: useQuery 및 getQueryKey 함수 개선
    - useQuery에서 hostname 대신 origin을 사용하도록 변경하여 기본값을 window.location.origin으로 설정함.
    - getQueryKey 함수에서 url 생성 시 origin을 사용하도록 수정함.
    - Api 타입에 triggerError 및 origin 필드를 추가하고, hostname 필드는 deprecated로 표시함.

--- package/library/lib-react-query/src/service/getQueryKey.ts
+++ package/library/lib-react-query/src/service/getQueryKey.ts
@@ -1,12 +1,17 @@
 type Api = {
+  /**
+   * @deprecated use origin instead
+   */
   hostname?: string;
+  origin?: string;
   pathname: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   body?: object;
 };
 
 const getQueryKey = (api: Api): string[] => {
-  const url = `${api.hostname}${api.pathname}`;
+  const origin = api.origin ?? api.hostname ?? window.location.origin;
+  const url = `${origin}${api.pathname}`;
   const body = JSON.stringify(api.body);
 
   return [url, api.method, body];
