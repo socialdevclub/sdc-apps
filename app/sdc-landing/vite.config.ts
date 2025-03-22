0c40885a45cb6594d74ea6ee2be872cb192f5e24
haian <ada.lovelace@kakao.com>
2025-03-23 03:47:21 +0900

    chore: Vite 설정 업데이트
    - envPrefix를 'PUBLIC'으로 설정하여 환경 변수 접두사를 지정함.
    - 서버 설정에 allowedHosts를 추가하여 'local.socialdev.club' 호스트를 허용함.

--- app/sdc-landing/vite.config.ts
+++ app/sdc-landing/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  envPrefix: 'PUBLIC',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,5 +11,8 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@pages': path.resolve(__dirname, './src/pages'),
     },
+  },
+  server: {
+    allowedHosts: ['local.socialdev.club'],
   },
 });
