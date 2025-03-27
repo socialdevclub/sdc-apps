84459f2462517f667371702ad8bf2254cff7f0d8
haian <ada.lovelace@kakao.com>
2025-03-28 02:56:35 +0900

    chore: tsconfig 설정 업데이트
    - allowImportingTsExtensions 옵션을 추가하여 TypeScript에서 .ts 확장자를 가진 파일을 임포트할 수 있도록 설정함.

--- app/sdc-landing/vite.config.ts
+++ app/sdc-landing/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
 import path from 'path';
 
 export default defineConfig({
   envPrefix: 'PUBLIC',
-  plugins: [react()],
+  plugins: [commonjs(), react({ jsxImportSource: '@emotion/react' })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
