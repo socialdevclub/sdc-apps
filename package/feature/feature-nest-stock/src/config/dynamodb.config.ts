1ae1ac979f99cb96ee7fddd115610e74f4c97e3e
haian <ada.lovelace@kakao.com>
2025-05-12 00:53:21 +0900

    fix: DynamoDB 설정에서 자격 증명 주석 처리
    - DynamoDB 설정 파일에서 AWS Lambda 환경에 따라 자격 증명 관련 코드를 주석 처리하여 유연성을 높였습니다. 이를 통해 Lambda 환경에서 실행될 때 자격 증명을 생략할 수 있도록 개선했습니다.

--- package/feature/feature-nest-stock/src/config/dynamodb.config.ts
+++ package/feature/feature-nest-stock/src/config/dynamodb.config.ts
@@ -1,12 +1,11 @@
 import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
-import { fromSSO } from '@aws-sdk/credential-providers';
 
 const { AWS_EXECUTION_ENV } = process.env;
 
 export const dynamoDBConfig: DynamoDBClientConfig = AWS_EXECUTION_ENV?.startsWith('AWS_Lambda_') // 람다 환경에서 돌아가는지?
   ? { region: 'ap-northeast-2' }
   : {
-      credentials: fromSSO({ profile: 'sdc-jhha' }),
+      // credentials: fromSSO({ profile: 'sdc-jhha' }),
       //   endpoint: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
       region: 'ap-northeast-2',
     };
