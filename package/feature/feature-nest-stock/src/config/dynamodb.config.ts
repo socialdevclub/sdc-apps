a124b1377668a1643450534e2b41d5aeac4b8766
haian <ada.lovelace@kakao.com>
2025-05-11 23:46:45 +0900

    fix: DynamoDB 설정에서 AWS Lambda 환경에 따른 자격 증명 처리 추가
    - DynamoDB 설정에서 AWS_EXECUTION_ENV 변수를 사용하여 Lambda 환경에서 실행되는 경우 자격 증명을 생략하도록 수정했습니다. 이를 통해 환경에 따라 적절한 설정을 적용하여 유연성을 높였습니다.

--- package/feature/feature-nest-stock/src/config/dynamodb.config.ts
+++ package/feature/feature-nest-stock/src/config/dynamodb.config.ts
@@ -1,11 +1,15 @@
 import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 import { fromSSO } from '@aws-sdk/credential-providers';
 
-export const dynamoDBConfig: DynamoDBClientConfig = {
-  credentials: fromSSO({ profile: 'sdc-jhha' }),
-  //   endpoint: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
-  region: 'ap-northeast-2',
-};
+const { AWS_EXECUTION_ENV } = process.env;
+
+export const dynamoDBConfig: DynamoDBClientConfig = AWS_EXECUTION_ENV?.startsWith('AWS_Lambda_') // 람다 환경에서 돌아가는지?
+  ? { region: 'ap-northeast-2' }
+  : {
+      credentials: fromSSO({ profile: 'sdc-jhha' }),
+      //   endpoint: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
+      region: 'ap-northeast-2',
+    };
 
 export const STOCK_TABLE_NAME = 'sdc-stock';
 export const STOCK_USER_TABLE_NAME = 'sdc-stock-user';
