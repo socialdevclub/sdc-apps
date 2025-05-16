23d7a6cb88d6e2deff05155222a283de3d714fe2
Nojun Park <pnj1118@naver.com>
2025-05-16 22:53:44 +0900

    feat: 대기 페이지 1차 완성

--- app/koi-client/src/page/@party@[partyId]/component/MainHeader.tsx
+++ app/koi-client/src/page/@party@[partyId]/component/MainHeader.tsx
@@ -1,15 +1,11 @@
-import { Button, Space } from 'antd';
 import { css } from '@linaria/core';
-import { useNavigate, useParams } from 'react-router-dom';
-import { ChevronLeft, RefreshCcw } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
+import styled from '@emotion/styled';
 import Header from '../../../component-presentation/Header';
 import ProfileValidator from '../../../component/ProfileValidator';
-import { Query } from '../../../hook';
 
 const PartyHeader = () => {
-  const { partyId } = useParams();
-  const { data: party } = Query.Party.useQueryParty(partyId ?? '');
-
   const navigate = useNavigate();
   // const [messageApi, contextHolder] = message.useMessage();
 
@@ -45,7 +41,6 @@ const PartyHeader = () => {
   return (
     <ProfileValidator>
       <Header
-        title={party?.title}
         LeftComponent={
           <ChevronLeft
             size={32}
@@ -61,44 +56,43 @@ const PartyHeader = () => {
             `}
           />
         }
-        RightComponent={
-          <Space>
-            {/* 대화 추천 상대 리스트 모달 버튼 */}
-            {/* 추천 상대 기능 정보 탭 하단으로 이동 Deprecated */}
-            {/* <SwitchCase
-              value={party?.activityId ?? ''}
-              caseBy={{
-                STOCK: (
-                  <RecommendedPartnersModal
-                    stockId={party?.activityName}
-                    trigger={<Button style={{ border: 'none' }} ghost icon={<Lightbulb color="white" />} />}
-                  />
-                ),
-              }}
-            /> */}
-            {/* 페이지 새로고침 버튼 */}
-            <Button
-              ghost
-              style={{ border: 'none' }}
-              icon={<RefreshCcw color="white" />}
-              onClick={() =>
-                (
-                  window.location as {
-                    reload: (isForceReload: boolean) => void;
-                  }
-                ).reload(true)
-              }
-            />
-            {/* 하이안: 잠깐 기능 닫아놈 */}
-            {/* <Dropdown menu={{ items }} trigger={['click']} placement="bottomRight">
-              <Button shape="circle" icon={<EllipsisOutlined />} />
-            </Dropdown> */}
-          </Space>
-        }
+        CenterComponent={<Title>방 만들기</Title>}
+        // RightComponent={
+        //   <Space>
+        //     {/* 대화 추천 상대 리스트 모달 버튼 */}
+        //     <SwitchCase
+        //       value={party?.activityId ?? ''}
+        //       caseBy={{
+        //         STOCK: (
+        //           <RecommendedPartnersModal
+        //             stockId={party?.activityName}
+        //             trigger={<Button style={{ border: 'none' }} ghost icon={<Lightbulb color="white" />} />}
+        //           />
+        //         ),
+        //       }}
+        //     />
+        //     {/* 페이지 새로고침 버튼 */}
+        //     <Button
+        //       ghost
+        //       style={{ border: 'none' }}
+        //       icon={<RefreshCcw color="white" />}
+        //       onClick={() => (window.location as { reload: (isForceReload: boolean) => void }).reload(true)}
+        //     />
+        //     {/* 하이안: 잠깐 기능 닫아놈 */}
+        //     {/* <Dropdown menu={{ items }} trigger={['click']} placement="bottomRight">
+        //       <Button shape="circle" icon={<EllipsisOutlined />} />
+        //     </Dropdown> */}
+        //   </Space>
+        // }
       />
       {/* {contextHolder} */}
     </ProfileValidator>
   );
 };
+
+const Title = styled.div`
+  font-size: 24px;
+  line-height: 135%;
+`;
 
 export default PartyHeader;
