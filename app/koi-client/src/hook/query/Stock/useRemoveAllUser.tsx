47a93aa1744170f043ae14a57a013e9d5a90f711
EunyoungLee <ey.lee@int2.us>
2025-05-17 16:58:54 +0900

    feat: useRemoveAllUser 쿼리 생성

---
+++ app/koi-client/src/hook/query/Stock/useRemoveAllUser.tsx
@@ -0,0 +1,17 @@
+import { useMutation } from 'lib-react-query';
+import { Request } from 'shared~type-stock';
+import { serverApiUrl } from '../../../config/baseUrl';
+
+const useRemoveUser = (stockId: string) => {
+  const { mutateAsync } = useMutation<Request.RemoveAllStockUser>({
+    api: {
+      hostname: serverApiUrl,
+      method: 'DELETE',
+      pathname: `/stock/user/all?stockId=${stockId}`,
+    },
+  });
+
+  return { mutateAsync };
+};
+
+export default useRemoveUser;
