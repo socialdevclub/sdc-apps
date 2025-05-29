4844d0ad4dd9d30a6c6b1f0c8f62a3926c5ec611
JOOWANHONG <vjvl95@naver.com>
2025-05-29 21:44:54 +0900

    fix : 상단 헤더 텍스트 기준으로 중앙정렬 완료

--- app/sdc-landing/src/common/Header/components/DesktopHeader/index.style.ts
+++ app/sdc-landing/src/common/Header/components/DesktopHeader/index.style.ts
@@ -1,13 +1,13 @@
 import styled from '@emotion/styled';
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
+  display: grid;
   padding: 13px 24px;
   position: sticky;
   top: 0;
   z-index: 1000;
   background-color: #000;
+  grid-template-columns: 1fr auto 1fr;
   align-items: center;
 `;
 
@@ -27,12 +27,15 @@ const HeaderLeft = styled.div``;
 const HeaderCenter = styled.div`
   display: flex;
   gap: 53px;
+  justify-content: center; /* ✅ 더 명확하게 중앙 정렬 */
+  place-self: center;
 `;
 
 const HeaderRight = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: flex-end;
 `;
 
 const HeaderItem = styled.span`
