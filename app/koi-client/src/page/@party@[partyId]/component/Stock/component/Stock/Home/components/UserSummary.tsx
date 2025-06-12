2bf96d0399c6753ead1785bb0c57cdc01dfbc361
haian <ada.lovelace@kakao.com>
2025-06-13 00:50:36 +0900

    fix: UserSummary 컴포넌트에서 Flex 제거 및 Card 컴포넌트 구조 개선

--- app/koi-client/src/page/@party@[partyId]/component/Stock/component/Stock/Home/components/UserSummary.tsx
+++ app/koi-client/src/page/@party@[partyId]/component/Stock/component/Stock/Home/components/UserSummary.tsx
@@ -1,4 +1,3 @@
-import { Flex } from 'antd';
 import { commaizeNumber } from '@toss/utils';
 import Card from '../../../../../../../../component-presentation/Card';
 import * as COLOR from '../../../../../../../../config/color';
@@ -28,28 +27,26 @@ const UserSummary = ({
   return (
     <>
       <MyLevel moneyRatio={moneyRatio} initialMoney={stock.initialMoney} />
-      <Flex gap={12} css={{ width: '100%' }}>
-        <Card
-          title="잔액"
-          value={`₩${commaizeNumber(user.money)}`}
-          valueColor={COLOR.pastelGreen}
-          rightComponent={
-            stock.isVisibleRank && users ? (
-              <>{users.sort((a, b) => b.money - a.money).findIndex((v) => v.userId === userId) + 1}위</>
-            ) : (
-              <></>
-            )
-          }
-        />
-        <Card
-          title="주식 가치"
-          valueColor={COLOR.pastelViolet}
-          value={`₩${commaizeNumber(allSellPrice)}`}
-          rightComponent={
-            stock.isVisibleRank ? <>{allUserSellPriceDesc().findIndex((v) => v.userId === userId) + 1}위</> : <></>
-          }
-        />
-      </Flex>
+      <Card
+        title="잔액"
+        value={`₩${commaizeNumber(user.money)}`}
+        valueColor={COLOR.pastelGreen}
+        rightComponent={
+          stock.isVisibleRank && users ? (
+            <>{users.sort((a, b) => b.money - a.money).findIndex((v) => v.userId === userId) + 1}위</>
+          ) : (
+            <></>
+          )
+        }
+      />
+      <Card
+        title="주식 가치"
+        valueColor={COLOR.pastelViolet}
+        value={`₩${commaizeNumber(allSellPrice)}`}
+        rightComponent={
+          stock.isVisibleRank ? <>{allUserSellPriceDesc().findIndex((v) => v.userId === userId) + 1}위</> : <></>
+        }
+      />
       <Card
         title="모두 팔고 난 뒤의 금액"
         value={`₩${commaizeNumber(user.money + allSellPrice)}`}
