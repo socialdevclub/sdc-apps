c8726d5820a7eb962af484f387bd8097d583dbbd
haian <ada.lovelace@kakao.com>
2025-06-22 23:48:04 +0900

    feat(sdc-landing): 헤더, 푸터, 더보기 버튼, 목표의 브랜드 협업 제거

--- app/sdc-landing/src/page/@/components/OurGoalSection/index.tsx
+++ app/sdc-landing/src/page/@/components/OurGoalSection/index.tsx
@@ -11,11 +11,11 @@ const imageUrls = [
     text: '누구나 즐길 수 있는\n게임 콘텐츠 설계',
     url: 'src/assets/img/Confetti.png',
   },
-  {
-    highlights: ['브랜드 협업'],
-    text: '다양한 인플루언서와\n브랜드 협업',
-    url: 'src/assets/img/Chat.png',
-  },
+  // {
+  //   highlights: ['브랜드 협업'],
+  //   text: '다양한 인플루언서와\n브랜드 협업',
+  //   url: 'src/assets/img/Chat.png',
+  // },
   {
     highlights: ['양방향 네트워킹'],
     text: '사람과 사람을 연결하는\n양방향 네트워킹',
@@ -28,8 +28,8 @@ export const OurGoalSection = () => {
     <styles.Container>
       <styles.Title>Our Goal</styles.Title>
       <styles.Description
-        text={`모두가 즐길 수 있는 체험형 게임을 개발하여\n사람과 사람을 연결하고,브랜드 협업을 통해\n 이색적인 콘텐츠 경험을 설계해요`}
-        highlights={['체험형 게임', '사람과 사람을 연결', '브랜드 협업', '이색적인 콘텐츠 경험']}
+        text={`모두가 즐길 수 있는 체험형 게임을 개발하여\n사람과 사람을 연결하고, 이색적인 콘텐츠 경험을 설계해요`}
+        highlights={['체험형 게임', '사람과 사람을 연결', '이색적인 콘텐츠 경험']}
       />
       <styles.CardWrapper>
         {imageUrls.map((imageUrl) => (
